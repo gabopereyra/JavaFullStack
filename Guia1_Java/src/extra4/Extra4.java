6191dcc0f8ec20543183782d6451f666a4ddb9b3
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-05-21 19:35:00 -0300

    Extra 4 added

--- Guia1_Java/src/extra4/Extra4.java
+++ Guia1_Java/src/extra4/Extra4.java
@@ -1,21 +1,52 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Elaborar un algoritmo en el cuál se ingrese un número entre 1 y 10 y se muestre su 
+equivalente en romano
  */
 package extra4;
 
-/**
- *
- * @author Gabriel
- */
+import java.util.Scanner;
+
 public class Extra4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un numero comprendido entre 1 y 10");
+        int num = read.nextInt();
+        while (num < 1 || num > 10) {
+            System.out.println("El número ingresado es incorrecto, intente nuevamente");
+            num = read.nextInt();
+        }
+        switch (num) {
+            case 1:
+                System.out.println("El equivalente es romano es: I");
+                break;
+            case 2:
+                System.out.println("El equivalente es romano es: II");
+                break;
+            case 3:
+                System.out.println("El equivalente es romano es: III");
+                break;
+            case 4:
+                System.out.println("El equivalente es romano es: IV");
+                break;
+            case 5:
+                System.out.println("El equivalente es romano es: V");
+                break;
+            case 6:
+                System.out.println("El equivalente es romano es: VI");
+                break;
+            case 7:
+                System.out.println("El equivalente es romano es: VII");
+                break;
+            case 8:
+                System.out.println("El equivalente es romano es: VIII");
+                break;
+            case 9:
+                System.out.println("El equivalente es romano es: IX");
+                break;
+            default:
+                System.out.println("El equivalente es romano es: X");
+                break;
+        } //END SWITCH
+    } //END MAIN
+} //END CLASS
