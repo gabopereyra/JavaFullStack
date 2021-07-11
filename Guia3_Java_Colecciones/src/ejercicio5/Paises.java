edbd15058c3f1be6f334d3847937372e7da60e0f
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-07-11 13:08:52 -0300

    Revert "Not necessaty file"
    This reverts commit 2d82bf79508c227fed7dab1ca6b36825f23c6e41.

---
+++ Guia3_Java_Colecciones/src/ejercicio5/Paises.java
@@ -0,0 +1,27 @@
+package ejercicio5;
+
+public class Paises {
+    private String pais;
+
+    public Paises() {
+    }
+
+    public Paises(String pais) {
+        this.pais = pais;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    @Override
+    public String toString() {
+        return pais;
+    }
+        
+    
+}
