eaaa6151f2a6087d24824df966c08b8b5b91d857
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-08-05 20:15:06 -0300

    Update exercise (another method to solve)

--- Guia2_Java_POO/src/ejercicio11/Ejercicio11.java
+++ Guia2_Java_POO/src/ejercicio11/Ejercicio11.java
@@ -11,31 +11,57 @@ Ejemplo fecha actual: Date fechaActual = new Date();
 package ejercicio11;
 
 import java.util.Date;
+import java.time.*;
+import java.time.Period;
 import java.util.Scanner;
 
 public class Ejercicio11 {
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        
+
         int dia;
         int mes;
         int anio;
-        
+
         System.out.println("Indique el dia (en numeros enteros):");
         dia = read.nextInt();
         System.out.println("Indique el mes (en numeros enteros):");
         mes = read.nextInt();
         System.out.println("Indique el año (en numeros enteros):");
         anio = read.nextInt();
-        
-        Date fechaCargada = new Date((anio-1900), (mes-1), dia);
-        System.out.println("La fecha cargada es la siguiente: "+fechaCargada);
-        
+
+        Date fechaCargada = new Date((anio - 1900), (mes - 1), dia);
+        System.out.println("La fecha cargada es la siguiente: " + fechaCargada);
+
         Date fechaActual = new Date();
+
+        System.out.println("La diferencia de años entre la fecha actual y la cargada es de: " + (fechaActual.getYear() - fechaCargada.getYear()) + " años.");
+        long resta = fechaActual.getTime() - fechaCargada.getTime();
+        //Date resta = new Date (fechaActual.getTime()-fechaCargada.getTime());
+        Date finalFecha = new Date(resta);
+
+        System.out.println("la resta es:" + resta);
+        System.out.println("La fecha final es: " + finalFecha.toString());
+        //System.out.println("La diferencia es: "+resta.getYear()+"anios"+resta.getMonth()+"meses"+resta.getDay()+"dias");
+
+        System.out.println("");
+        System.out.println("Ahora vamos a usar el otro metodo (TIME)");
+
+        System.out.println("La fecha cargada es: " + LocalDate.of(anio, mes, dia));
+        Period period = Period.between(LocalDate.of(anio, mes, dia), LocalDate.now());
+
+        int years = Math.abs(period.getYears());
+        int months = Math.abs(period.getMonths());
+        int days = Math.abs(period.getDays());
+        System.out.println("Diference is : " + years + " year, " + months + " months, " + days + " days");
+
+        System.out.println(period);
         
-        System.out.println("La diferencia de años entre la fecha actual y la cargada es de: "+(fechaActual.getYear()-fechaCargada.getYear())+" años.");
-        
+        // DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+        // System.out.println("\nFecha ingresada: " + formatoFecha.format(fecha1));
+
+        //
     } //End main
-    
+
 }
