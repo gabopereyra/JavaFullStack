bc20be6435c762992f8a6577c215138396781248
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-10-23 17:19:13 -0300

    ejemplos de prueba con Spring

---
+++ demoProbando/demoProbando/src/main/java/com/ejemploMiercoles/demoProbando/DemoProbandoApplication.java
@@ -0,0 +1,13 @@
+package com.ejemploMiercoles.demoProbando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProbandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProbandoApplication.class, args);
+	}
+
+}
---
+++ demoProbando/demoProbando/src/test/java/com/ejemploMiercoles/demoProbando/DemoProbandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemploMiercoles.demoProbando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProbandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
